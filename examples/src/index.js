23ce6a5ca0b3c82f89c900bd21f9f3f9677b7456
hazemmadkour <hazemadkour2007@gmail.com>
2021-01-01 04:18:47 +0200

    feat: add examples folder

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {KeyShortcutProvider} from 'react-key-shortcuts';
-import App from './environment/App';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
